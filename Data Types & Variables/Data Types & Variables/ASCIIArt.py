0f86da5f43b1038360ff1d415a4d5993ae29e4bc
Johnny Olmedo <107173906+johnny-hacker@users.noreply.github.com>
2023-11-12 12:42:07 +0100

    Add files via upload

---
+++ Data Types & Variables/Data Types & Variables/ASCIIArt.py
@@ -0,0 +1,13 @@
+# Module 1 Option 1
+# Programmer: Olmedo, Johnny
+# ITS320
+# Submission date: 11/14/2021 
+# This program prints a picture of a mouse  
+print ('''      
+                (\-.
+                / _'>
+        _)     / _)=
+       (      / _/
+       '-.___(___)_ ''')
+
+print("okay \a", "are you ok")   
