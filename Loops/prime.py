9f7b0b09a53d1609d204f1647ff3310ede088bda
Johnny Olmedo <107173906+johnny-hacker@users.noreply.github.com>
2023-11-13 12:54:09 +0100

    Determines if number is Prime
    Uses for loops and functions

---
+++ Loops/prime.py
@@ -0,0 +1,25 @@
+# Programmer: Olmedo, Johnny
+# Submission date: 10/05/2019 
+# This program determines the prime numbers from 1-100
+
+def main():
+    # the range wil be 1 - 100
+    prime = int in range(1, 100)
+    # call isPrime function
+    isPrime(prime)
+    # display the prie numbers
+    print("the prime numbers from 1 through 100 are : ", isPrime(prime))
+
+
+def isPrime(prime):
+    if prime % integerList() != 0:  # determine if the number modulo integerList equals to 0
+        return prime  # if number modulo i does not equal to 0 the number is a prime number
+
+
+def integerList():
+    for i in (2, 101):
+        return i
+        # call main
+
+
+main()
